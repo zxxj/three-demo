c347717aef26ff3df6b184f459a73bf817ed5a6e
zhangxinxin <1285367184@qq.com>
2024-01-05 17:27:01 +0800

    ✨feat: 几何体顶点位置数据和点模型

---
+++ 二.几何体BufferGeometry/01_几何体顶点位置数据和点模型/model.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+// 创建一个空的几何对象
+const geometry = new THREE.BufferGeometry();
+
+// 定义类型化数组用于创建顶点
+const vertices = new Float32Array([
+  0,
+  0,
+  0, //顶点1坐标
+  50,
+  0,
+  0, //顶点2坐标
+  0,
+  30,
+  0, //顶点3坐标
+  0,
+  0,
+  30, //顶点4坐标
+  0,
+  0,
+  50, //顶点5坐标
+  10,
+  30,
+  30, //顶点6坐标
+]);
+
+// 创建属性缓冲区对象
+const attribue = new THREE.BufferAttribute(vertices, 3); // 3个为一组,表示一个顶点的xyz坐标
+
+// 设置几何顶点
+geometry.attributes.position = attribue;
+
+// 点渲染模式
+const material = new THREE.PointsMaterial({ color: 0xffff00 });
+material.size = 30;
+
+const points = new THREE.Points(geometry, material);
+
+export default points;
