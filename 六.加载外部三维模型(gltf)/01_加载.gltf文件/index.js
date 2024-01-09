3fbd0385f15cd8d4d422a83c8af992b660364ca3
zhangxinxin <1285367184@qq.com>
2024-01-09 17:35:00 +0800

    ✨feat: 记载外部三维模型

---
+++ 六.加载外部三维模型(gltf)/01_加载.gltf文件/index.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Mesh from './model.js';
+
+// 场景
+const scene = new THREE.Scene();
+scene.add(Mesh);
+
+// 相机
+const camera = new THREE.PerspectiveCamera(
+  30,
+  window.innerWidth / window.innerHeight,
+  1,
+  3000
+);
+camera.position.set(150, 150, 150);
+camera.lookAt(30, 0, 0);
+
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(200, 400, 50);
+scene.add(directionalLight);
+
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const render = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(render);
+};
+
+render();
+
+//解决加载gltf格式模型纹理贴图和原图不一样问题
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+// 坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
+
+// 轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(30, 0, 0);
+controls.update();
+
+document.body.appendChild(renderer.domElement);
+
+// 自适应宽高
+window.onresize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+};
---
+++ 六.加载外部三维模型(gltf)/01_加载.gltf文件/model.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const group = new THREE.Group();
+
+// 1.实例化一个加载器
+const loader = new GLTFLoader();
+
+// 2.加载gltf模型
+loader.load('../工厂.gltf', (gltf) => {
+  console.log(gltf);
+  // 返回的场景对象gltf.scene插入到threejs场景中
+  group.add(gltf.scene);
+});
+
+export default group;
