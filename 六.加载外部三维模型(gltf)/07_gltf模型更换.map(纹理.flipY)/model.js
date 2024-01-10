1c348bf6bcc7b0c73b7821392f358cbc8d285855
zhangxinxin <1285367184@qq.com>
2024-01-10 23:40:06 +0800

    ✨feat: 加载外部三维模型

--- 六.加载外部三维模型(gltf)/07_gltf模型更换.map(纹理.flipY)/model.js
+++ 六.加载外部三维模型(gltf)/07_gltf模型更换.map(纹理.flipY)/model.js
@@ -3,14 +3,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const group = new THREE.Group();
 
-// 1.实例化一个加载器
 const loader = new GLTFLoader();
 
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('./黑色.png');
 texture.flipY = false; //是否翻转纹理贴图
 
-// 2.加载gltf模型
 loader.load('./手机模型.glb', (gltf) => {
   group.add(gltf.scene);
 
